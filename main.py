d022ede5833ee4a88368e69e19a69b980df40c65
Heye Everts <heye.everts.1@gmail.com>
2021-12-29 12:51:05 +0000

    add all commands, split long discord messages, error handling

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 import sys
+from typing import List
 from rcon import Client
 import discord
 from config import Config
 
 def run_rcon(cmd: str) -> str:
-    with Client(Config.rcon_ip, int(Config.rcon_port), passwd=Config.rcon_passwd) as client:
-        response = client.run(cmd)
+    try:
+        with Client(Config.rcon_ip, int(Config.rcon_port), passwd=Config.rcon_passwd) as client:
+            response = client.run(cmd)
 
-    print("RCON RESPONSE: " + str(response))
-    return str(response)
+        print("RCON RESPONSE: " + str(response))
+        return str(response)
+    except:
+        return "error"
+
+
+def split_lines2000(input: str) -> List[str]:
+    blocks = []
+    one_line = ""
+    for line in input.splitlines():
+        if len(one_line + line) < 2000:
+            one_line += line + "\n"
+        else:
+            blocks.append(one_line)
+            one_line = ""
+
+    if one_line:
+        blocks.append(one_line)
+
+    return blocks
 
 
 def main(argv) -> int:
@@ -16,7 +36,44 @@ def main(argv) -> int:
     Config.load(argv)
 
     allowed_commands = [
-        "save"]
+        "addalltowhitelist",
+        "additem",
+        "adduser",
+        "addusertowhitelist",
+        "addvehicle",
+        "addxp",
+        "alarm",
+        "banid",
+        "banuser",
+        "changeoption",
+        "chopper",
+        "createhorde",
+        "godmod",
+        "gunshot",
+        "help",
+        "invisible",
+        "kickuser",
+        "noclip",
+        "players",
+        "quit",
+        "releasesafehouse",
+        "reloadlua",
+        "reloadoptions",
+        "removeuserfromwhitelist",
+        "removezombies",
+        "replay",
+        "save",
+        "sendpulse",
+        "servermsg",
+        "setaccesslevel",
+        "showoptions",
+        "startrain",
+        "stoprain",
+        "teleport",
+        "teleportto",
+        "unbanid",
+        "unbanuser",
+        "voiceban"]
 
     client = discord.Client()
 
@@ -36,7 +93,11 @@ def main(argv) -> int:
                 if message.content.startswith(one_cmd):
                     print("COMMAND: " + message.content)
                     reply = run_rcon(message.content)
-                    await message.channel.send(reply)
+                    if len(reply) < 2000:
+                        await message.channel.send(reply)
+                    else:
+                        for line in split_lines2000(reply):
+                            await message.channel.send(line)
                     return
 
             await message.channel.send("unknown command " + message.content)
